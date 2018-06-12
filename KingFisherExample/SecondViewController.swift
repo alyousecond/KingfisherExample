96570ba79c1a338c7c85db74a174a27fd74f578a
Yuji Sugaya <sugaya@ivalueworks.co.jp>
2018-06-12 10:56:28 +0900

    Add project file for Xcode

---
+++ KingFisherExample/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  KingFisherExample
+//
+//  Created by Yuji Sugaya on 2018/06/12.
+//  Copyright © 2018年 Yuji Sugaya. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ KingFisherExampleTests/KingFisherExampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  KingFisherExampleTests.swift
+//  KingFisherExampleTests
+//
+//  Created by Yuji Sugaya on 2018/06/12.
+//  Copyright © 2018年 Yuji Sugaya. All rights reserved.
+//
+
+import XCTest
+@testable import KingFisherExample
+
+class KingFisherExampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
